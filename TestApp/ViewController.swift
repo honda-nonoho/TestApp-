7ccdcaae2c829e94155de4dfb48c635960cdff80
HondaNonoho <nnhhnd3012@gmail.com>
2020-05-25 17:20:14 +0900

    追加

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -13,8 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var secondTitleLabel: UILabel!
+    @IBOutlet weak var thirdTitleLabel: UILabel!
+    
+    @IBAction func textLabel(_ sender: Any) {
+    }
+    @IBAction func secondTextLabel(_ sender: Any) {
+    }
+    @IBAction func thirdTextLabel(_ sender: Any) {
+    }
+    
+    @IBAction func tappedButton(_ sender: Any) {
+    }
 }
 
